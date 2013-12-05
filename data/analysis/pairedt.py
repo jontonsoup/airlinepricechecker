b93d118dd83734fdc791ab5a1caeceab38d320ad
Angela Jiang <angelajiang2014@u.northwestern.edu>
2013-12-04 21:22:19 -0600

    Performs ttest. Tests for normality and get mean for tor and firefox arrays

--- data/analysis/pairedt.py
+++ data/analysis/pairedt.py
@@ -1,28 +1,58 @@
 import json
 import sys
+import array
+import numpy as np
+import scipy.stats as stats
 from math import fabs
 from datetime import datetime
-from scipy import stats
 from pprint import pprint
 
 #Needs two arrays to put into stats.ttest_rel(a,b)
 
-def pairedt(json_file):
+def pairedt(pairs):
+    #pairs = getCostPairs(json_file)
+    fpairs = pairs['firefox']
+    tpairs = pairs['tor']
+    #metadata  holds various stats for both arrays
+    metadata = dict(firefox = dict(), tor = dict())
+    fstats = metadata['firefox']
+    tstats = metadata['tor']
+
+    #Normality check
+    nt = stats.normaltest(fpairs)
+    fstats['normal_p'] = nt[1]
+    nt = stats.normaltest(tpairs)
+    tstats['normal_p'] = nt[1]
+    #Mean
+    fstats['mean'] = np.mean(fpairs)
+    tstats['mean'] = np.mean(tpairs)
+    #ttest
+    results = stats.ttest_rel(fpairs, tpairs)
+    metadata['ttest_p'] = results[1]
+    return metadata
+    
+
+def getCostPairs(json_file):
     f = open(json_file, 'r')
     data = json.load(f)
     f.close()
     fArray = [ x for x in data if x['browser'] == 'Firefox']
     tArray = [ x for x in data if x['browser'] == 'Tor']
     pairCount = 0
+    pairs = dict(tor=[], firefox=[])
     for tt in tArray:
         tt['paired'] = 0
     for ff in fArray:
         for tt in tArray:
             if (not tt['paired']):
-                if ff['date'] == tt['date'] and withinMinutes(5, ff['time'], tt['time'], '%H:%M:%S'):
-                    if ff['airlineCode'] == tt['airlineCode'] and ff['flightNum'] == tt['flightNum']:
-                        tt['paired'] = 1
-                        pairCount += 1
+                if isPaired(tt, ff):
+                    tt['paired'] = 1
+                    tPairs = pairs['tor']            
+                    fPairs = pairs['firefox']
+                    tPairs.append(tt['price'])
+                    fPairs.append(ff['price'])
+                    pairCount += 1
+    return pairs
 
 def isPaired(t, ff):
     if ff['date'] == t['date'] and withinMinutes(5, ff['time'], t['time'], '%H:%M:%S'):
@@ -44,6 +74,6 @@ def withinMinutes(minutes, tstr1, tstr2, tformat):
     
 
 if __name__ == '__main__':
-    pairedt('edited-flight_output.json') 
+    pairedt('cut-flight_output.json') 
     
      
